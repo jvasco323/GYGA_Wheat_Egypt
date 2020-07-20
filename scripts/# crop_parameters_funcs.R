31182d9fd3c1e0c98068df3256327f82ef099646
Languillaume, Antoine <antoine.languillaume@wur.nl>
2020-07-20 16:58:05 +0200

    WIP update TSUM routine

--- scripts/# crop_parameters_funcs.R
+++ scripts/# crop_parameters_funcs.R
@@ -62,60 +62,106 @@ extract_nearest <- function(x, target){
 #' Extract julian days corresponding to TSUM, given a conversion table.
 #'
 #' @param TSUM tibble, data.frame or numeric vector containing the TSUM to convert in julian days.
-#' @param dtemp tibble or data.frame containing both Julian days (column "juld") and temperature sum (column "TSUM").
+#' @param dtemp tibble or data.frame containing both Julian days (column "csd") and temperature sum (column "TSUM").
 #'
 #' @importFrom tibble is_tibble
 #' @importFrom purrr map2 reduce
 
-dd_to_juld <- function(TSUM, dtemp){
+dd_to_csd <- function(TSUM, dtemp){
   
-  dd_to_juld_atom <- function(TSUM, dtemp) {
+  dd_to_csd_atom <- function(TSUM, dtemp) {
     
-    dtemp[which_nearest(TSUM, dtemp[,"TSUM"]), "juld"]
+    dtemp[which_nearest(TSUM, dtemp[,"TSUM"]), "csd"]
   }
   
   if(is_tibble(TSUM) || is.data.frame(TSUM)){
     TSUM <- deframe(TSUM)
   }
   
-  if(length(TSUM) != length(unique(dtemp$Year))){
+  if(length(TSUM) != length(unique(dtemp$crop_year))){
     stop("The number of elements in TSUM must be equal to the number of years in dtemp")
   }
   
   map2(.x = TSUM,
-       .y = split(dtemp, dtemp$Year),
-       ~ dd_to_juld_atom(TSUM = .x , dtemp = .y)) %>%
+       .y = split(dtemp, dtemp$crop_year),
+       ~ dd_to_csd_atom(TSUM = .x , dtemp = .y)) %>%
     reduce(rbind)
   
 }
 
 #' Extract TSUM corresponding to julian days, given a conversion table.
 #'
-#' @param juld tibble, data.frame or numeric vector containing the julian days to convert in TSUM.
-#' @param dtemp tibble or data.frame containing both Julian days (column "juld") and temperature sum (column "TSUM").
+#' @param csd tibble, data.frame or numeric vector containing the julian days to convert in TSUM.
+#' @param dtemp tibble or data.frame containing both Julian days (column "csd") and temperature sum (column "TSUM").
 #'
 #' @importFrom tibble is_tibble
 #' @importFrom purrr map2 reduce
 
 
-juld_to_dd <- function(juld, dtemp){
+csd_to_dd <- function(csd, dtemp){
   
-  juld_to_dd_atom <- function(juld, dtemp) {
+  csd_to_dd_atom <- function(csd, dtemp) {
     
-    dtemp[which_nearest(juld, dtemp[,"juld"]), "TSUM"]
+    dtemp[which_nearest(csd, dtemp[,"csd"]), "TSUM"]
   }
   
-  if(is_tibble(juld) || is.data.frame(juld)){
-    juld <- deframe(juld)
+  if(is_tibble(csd) || is.data.frame(csd)){
+    csd <- deframe(csd)
   }
   
-  if(length(juld) != length(unique(dtemp$Year))){
-    stop("The number of elements in juld must be equal to the number of years in dtemp")
+  if(length(csd) != length(unique(dtemp$crop_year))){
+    stop("The number of elements in csd must be equal to the number of years in dtemp")
   }
   
-  map2(.x = juld,
-       .y = split(dtemp, dtemp$Year),
-       ~ juld_to_dd_atom(juld = .x , dtemp = .y)) %>%
-    reduce(rbind)
+  map2_dbl(.x = csd,
+           .y = split(dtemp, dtemp$crop_year),
+           ~ csd_to_dd_atom(csd = .x , dtemp = .y))
   
+}
+
+
+compute_dd <- function(dtemp_year) {
+  
+  dtemp_year$TSUM <- vector("numeric", nrow(dtemp_year))
+  for (i in 1:length(dtemp_year$csd)) {
+    
+    ### Before emergence
+    if(dtemp_year$TSUM[i] < TSUMEM) {
+      
+      avg_temp <- (dtemp_year$TMAX[i] + dtemp_year$TMIN[i]) / 2
+      
+      # Correct for max efficient temperature for emergence
+      if(avg_temp >= TEFFMX) {
+        avg_temp <- TEFFMX
+      }
+      
+      ## Degree days
+      dtemp_year$dd[i] <- avg_temp - TBASEM
+      
+      ## Temperature sum
+      # Handle initial condition
+      if(length(dtemp_year$TSUM[i-1]) == 0) { # First day
+        dtemp_year$TSUM[i] <- dtemp_year$dd[i]
+      } else { # Rest of the days
+        dtemp_year$TSUM[i] <- dtemp_year$TSUM[i-1] + dtemp_year$dd[i]
+      }
+      
+      ### After emergence
+    } else {
+      
+      avg_temp <- (dtemp_year$TMAX[i] + dtemp_year$TMIN[i]) / 2
+      
+      # Correct for max efficient temperature for development
+      # Tmax is derived from DTSMTB (last value)
+      if(avg_temp >= Tmax) {
+        avg_temp <- Tmax
+      }
+      
+      ## Degree days
+      dtemp_year$dd[i] <- avg_temp - TBASE
+      ## Temperature sum
+      dtemp_year$TSUM[i] <- dtemp_year$TSUM[i-1] + dtemp_year$dd[i]
+    }
+  }
+  return(dtemp_year)
 }
--- scripts/6. Crop Parameters (TO DO).R
+++ scripts/6. Crop Parameters (TO DO).R
@@ -18,10 +18,18 @@ tictoc::tic()
 
 # Sowing dates
 sowing_date <- read.csv("./data/crop-management/sowing_date.csv")
+# Rename year crop_year, year corresponds here to the cropping season
+# i.e. year were harvest took place
+names(sowing_date)[grep("year", names(sowing_date))] <- "crop_year"
+sowing_date$year <- as.numeric(str_extract(sowing_date$sowing, "\\d{4}$"))
 
 # Read crop data
 TSUMEM <- 120
+TBASEM <- 0
+TEFFMX <- 30
+
 TBASE <- 0
+Tmax <- 30
 # DTSMTB:
 #     - [ 0.0,  0.0,
 #         30.0, 30.0,
@@ -34,10 +42,14 @@ TBASE <- 0
 # ================================================================================================== #
 
 # Nile Delta
-weather_nile_delta <- read_excel("./data/wofost-inputs/weather_nasa_NileDelta.xlsx", sheet = "Sheet1")
+weather_nile_delta <- read_excel("./data/wofost-inputs/weather_nasa_NileDelta.xlsx", 
+                                 sheet = "Sheet1")
 day <- as.Date(weather_nile_delta$DAY)
-days <- data.frame(DAY = weather_nile_delta$DAY, Year = as.numeric(format(day, format = "%Y")), Month = as.numeric(format(day, format = "%m")), Day = as.numeric(format(day, format = "%d")))
-weather_nile_delta <- merge(weather_nile_delta, days, by='DAY')
+days <- data.frame(DAY = weather_nile_delta$DAY, 
+                   Year = as.numeric(format(day, format = "%Y")), 
+                   Month = as.numeric(format(day, format = "%m")), 
+                   Day = as.numeric(format(day, format = "%d")))
+weather_nile_delta <- merge(weather_nile_delta, days, by = 'DAY')
 
 # Duration provided by agronomists
 intD1 <- c(80, 105)
@@ -46,33 +58,117 @@ D1 <- ceiling(mean(intD1))
 D2 <- ceiling(mean(intD2))
 
 # Select only time and temperature variables in weather data
-dtemp <- weather_nile_delta %>% filter(Year %in% sowing_date$year) %>% select(Year, Month, Day, TMIN, TMAX)
+dtemp <- weather_nile_delta %>% 
+  filter(Year %in% sowing_date$year) %>% # DO NOT FORGET TO UNCOMMENT
+  select(Year, Month, Day, TMIN, TMAX)
+
+# Count number of missing days per year
+years <- unique(dtemp$Year)
+nb_data <- dtemp %>%
+  split(.$Year) %>%
+  map_dbl(nrow)
+nb_real <- ifelse(lubridate::leap_year(years), 366, 365)
+missing_dtemp <- tibble(years, nb_data, nb_real) %>%
+  mutate(nb_missing = nb_real - nb_data) %>%
+  as.data.frame()
 
 # Add a column with date in Julian days
-dtemp <- dtemp %>% mutate(juld = paste(Year, Month, Day, sep = "-") %>% as_date() %>% yday())
+dtemp <- dtemp %>% 
+  mutate(juld = paste(Year, Month, Day, sep = "-") %>% 
+           as_date() %>% 
+           yday())
+
+dtemp[] <- lapply(dtemp, as.numeric)
+tail(dtemp)
+
+
+#  The sowing date correspond to the first Cropping Season Day (csd)
+sowing_date$csd <- 1
+
+# Include cropping season information in weather data.frame: crop_year and csd
+
+# foo <- inner_join(dtemp, 
+#                   select(sowing_date, year, crop_year),
+#                   by = c("Year" = "year"))
+# 
+# tail(foo)
+
+# This must be done one a per year basis
+
+# temp names
+a <- split(dtemp, dtemp$Year)
+b <- split(sowing_date, sowing_date$year)
+
+l_dtemp <- map2(
+  a,
+  b,
+  ~ full_join(.x, 
+              select(.y, doy, csd, crop_year), by =  c("juld" = "doy"))
+  )
+dtemp <- reduce(l_dtemp, rbind)
+
+# So far all csd are NA but the sowing data
+# Convert those NA to zeros
+dtemp$csd[is.na(dtemp$csd)] <- 0
+
+
+tail(dtemp)
+# Increment missing csd and crop_year 
+
+ref_year <- min(dtemp$crop_year, na.rm = TRUE) - 1
+dtemp$crop_year[1] <- ref_year
+for (i in 2:nrow(dtemp)) {
+  
+  if(dtemp$csd[i-1] != 0 & dtemp$csd[i] != 1) {
+    dtemp$csd[i] <- dtemp$csd[i-1] + 1
+  }
+  
+  if(grepl("^\\d{4}$", dtemp$crop_year[i])) {
+    ref_year <- dtemp$crop_year[i]
+  } 
+  dtemp$crop_year[i] <- ref_year
+}
+
+
+dtemp[310:330, ]
+sowing_date
 
 # Degree days calculation
-dtemp[] <- lapply(dtemp, as.numeric)
-dtemp$dd <- ((dtemp$TMAX + dtemp$TMIN) / 2) - TBASE
+# Exclude problematic years for the moment
+dtemp <- dtemp[!(dtemp$crop_year %in% c(2008, 2018, 2019)), ]
+sowing_date <- sowing_date[!(sowing_date$crop_year %in% c(2018, 2019)), ]
 
-# Compute yearly temperature sums
-dtemp <- dtemp %>% group_by(Year) %>% mutate(TSUM = cumsum(dd)) %>% ungroup()
+dtemp %>%
+  filter(crop_year %in% sowing_date$crop_year) %>%
+  split(.$crop_year)%>%
+  map(dim)
+
+
+
+dtemp <- dtemp %>%
+  split(.$crop_year) %>%
+  map(compute_dd) %>%
+  reduce(rbind)
+  
 
 # Process weather data and tsums
-TSUM_sowing <- juld_to_dd(sowing_date$doy, dtemp)
-TSUM_emergence <- TSUM_sowing + TSUMEM
-date_emergence <- dd_to_juld(TSUM_emergence, dtemp)
+TSUM_sowing <- csd_to_dd(sowing_date$csd, dtemp)
+TSUM_sowing
+
+date_emergence <- dd_to_csd(rep(TSUMEM, length(unique(dtemp$crop_year))), dtemp)
+
 date_anthesis <- date_emergence + D1 
-TSUM_anthesis <- juld_to_dd(date_anthesis, dtemp)
+TSUM_anthesis <- csd_to_dd(date_anthesis, dtemp)
+
 date_maturity <- date_anthesis + D2
-TSUM_maturity <- juld_to_dd(date_maturity, dtemp)
-TSUM_anthesis - TSUM_sowing %>% deframe() %>% mean()
+TSUM_maturity <- csd_to_dd(date_maturity, dtemp)
 
 # Calculate TSUM1 and TSUM2
-TSUM1 <- TSUM_anthesis - TSUM_emergence
-TSUM2 <- TSUM_maturity - TSUM_anthesis
-TSUM <- tibble(TSUM1 = deframe(TSUM_anthesis - TSUM_emergence), TSUM2 = deframe(TSUM_maturity - TSUM_anthesis))
-tidy_TSUM <- TSUM %>% tidyr::gather(key = "Parameter", value = "TSUM")
+TSUM <- tibble(TSUM1 = TSUM_anthesis - TSUMEM, 
+               TSUM2 = TSUM_maturity - TSUM_anthesis)
+tidy_TSUM <- tidyr::pivot_longer(data = TSUM, 
+                                 cols = c("TSUM1", "TSUM2"),
+                                 names_to = "Parameter", values_to = "TSUM")
 mean_TSUMs <- map(TSUM, ~ round(mean(.x), digits = 0))
 
 # Plot temperature sums
@@ -81,7 +177,9 @@ hist_TSUM <- ggplot(tidy_TSUM)+
     geom_histogram(binwidth = 10, colour = "black")+
     xlab(expr(tidy_TSUM ~ degree*C.day^{-1})) +
     theme_bw()
-TSUM_mean <- tidy_TSUM %>% group_by(Parameter) %>% summarise(value = round(mean(TSUM), digits = 0))
+TSUM_mean <- tidy_TSUM %>% 
+  group_by(Parameter) %>% 
+  summarise(value = round(mean(TSUM), digits = 0), .groups = "drop")
 hist_TSUM + geom_vline(xintercept = TSUM_mean$value, linetype = "dashed", size = 1)
 
 # Save plot to pdf
@@ -95,7 +193,10 @@ hist_TSUM + geom_vline(xintercept = TSUM_mean$value, linetype = "dashed", size =
 # Upper Egypt
 weather_upper_egypt <- read_excel("./data/wofost-inputs/weather_nasa_UpperEgypt.xlsx", sheet = "Sheet1")
 day <- as.Date(weather_upper_egypt$DAY)
-days <- data.frame(DAY = weather_upper_egypt$DAY, Year = as.numeric(format(day, format = "%Y")), Month = as.numeric(format(day, format = "%m")), Day = as.numeric(format(day, format = "%d")))
+days <- data.frame(DAY = weather_upper_egypt$DAY, 
+                   Year = as.numeric(format(day, format = "%Y")), 
+                   Month = as.numeric(format(day, format = "%m")), 
+                   Day = as.numeric(format(day, format = "%d")))
 weather_upper_egypt <- merge(weather_upper_egypt, days, by='DAY')
 
 # Duration provided by agronomists
